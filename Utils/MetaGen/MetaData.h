57ff01499b63092466c701822aafdb38d21eda0d
Wildboar <trevorgeek@gmail.com>
2025-04-11 12:31:59 +0000

    Generate output data

--- Utils/MetaGen/MetaData.h
+++ Utils/MetaGen/MetaData.h
@@ -12,25 +12,29 @@ using namespace std;
 #include <opencv2/opencv.hpp>
 using namespace cv;
 
-namespace name
+namespace NVL_App
 {
     class MetaData
     {
     private:
         double _focal;
         Size _imageSize;
-        Vec3d _rvec;
-        Vec3d _tvec;
+        Vec3d _rvec_1;
+        Vec3d _tvec_1;
+        Vec3d _rvec_2;
+        Vec3d _tvec_2;
         int _blockSize;
         int _gridSize;
     public:
-        MetaData(double focal, Size imageSize, Vec3d rvec, Vec3d tvec, int blockSize, int gridSize)
-            : _focal(focal), _imageSize(imageSize), _rvec(rvec), _tvec(tvec), _blockSize(blockSize), _gridSize(gridSize) {}
+        MetaData(double focal, Size imageSize, Vec3d rvec_1, Vec3d tvec_1, Vec3d rvec_2, Vec3d tvec_2, int blockSize, int gridSize)
+            : _focal(focal), _imageSize(imageSize), _rvec_1(rvec_1), _tvec_1(tvec_1), _rvec_2(rvec_2), _tvec_2(tvec_2), _blockSize(blockSize), _gridSize(gridSize) {}
 
         double GetFocal() { return _focal; }
         Size& GetImageSize() { return _imageSize; }
-        Vec3d& GetRVec() { return _rvec; }
-        Vec3d& GetTVec() { return _tvec; }
+        Vec3d& GetRVec_1() { return _rvec_1; }
+        Vec3d& GetTVec_1() { return _tvec_1; }
+        Vec3d& GetRVec_2() { return _rvec_2; }
+        Vec3d& GetTVec_2() { return _tvec_2; }
         int GetBlockSize() { return _blockSize; }
         int GetGridSize() { return _gridSize; }
     };
--- Utils/MetaGen/Source.cpp
+++ Utils/MetaGen/Source.cpp
@@ -10,6 +10,8 @@
 using namespace std;
 
 #include <NVLib/Logger.h>
+#include <NVLib/RandomUtils.h>
+#include <NVLib/StringUtils.h>
 #include <NVLib/Path/PathHelper.h>
 #include <NVLib/Parameters/Parameters.h>
 
@@ -23,6 +25,10 @@ using namespace cv;
 // Function Prototypes
 //--------------------------------------------------
 void Run(NVLib::Parameters * parameters);
+unique_ptr<NVL_App::MetaData> CreateMetaData();
+Vec3d GetRandomRotation();
+Vec3d GetRandomTranslation();
+void Save(const string& path, NVL_App::MetaData * metaData);
 
 //--------------------------------------------------
 // Execution Logic
@@ -36,6 +42,8 @@ void Run(NVLib::Parameters * parameters)
 {
     if (parameters == nullptr) return; auto logger = NVLib::Logger(1);
 
+    NVLib::RandomUtils::TimeSeedRandomNumbers();
+
     logger.StartApplication();
 
     logger.Log(1, "Loading parameters");
@@ -43,7 +51,91 @@ void Run(NVLib::Parameters * parameters)
     auto dataset = NVL_Utils::ArgReader::ReadString(parameters, "dataset");
     auto pathHelper = NVLib::PathHelper(database, dataset);
 
+    logger.Log(1, "Creating metadata");
+    auto metaData = CreateMetaData();
+
+    logger.Log(1, "Saving metadata");
+    auto name = NVLib::StringUtils::GetDateTimeString();
+    auto path = pathHelper.GetPath("MetaData", name + ".xml");
+    Save(path, metaData.get());
+
     logger.StopApplication();
+}
+
+//--------------------------------------------------
+// Generate Data
+//--------------------------------------------------
+
+/**
+ * Create the metadata for the application
+ * @return The metadata object
+ */
+unique_ptr<NVL_App::MetaData> CreateMetaData() 
+{
+    auto focal = 640.0 * (1.0 + NVLib::RandomUtils::GetInteger(0, 100) / 100.0);
+    auto imageSize = Size(640, 480);
+
+    auto rvec_1 = GetRandomRotation();
+    auto tvec_1 = GetRandomTranslation();
+    auto rvec_2 = GetRandomRotation();
+    auto tvec_2 = GetRandomTranslation();
+    
+    auto blockSize = 5;
+    auto gridSize = 10;
+
+    return make_unique<NVL_App::MetaData>(focal, imageSize, rvec_1, tvec_1, rvec_2, tvec_2, blockSize, gridSize);
+}
+
+/**
+ * Get a random rotation vector
+ * @return The random rotation vector
+ */
+Vec3d GetRandomRotation() 
+{
+    auto rx = NVLib::RandomUtils::GetInteger(0, 20) / 100.0;
+    auto ry = NVLib::RandomUtils::GetInteger(0, 20) / 100.0;
+    auto rz = NVLib::RandomUtils::GetInteger(80, 100) / 100.0;
+    auto rmag_r = sqrt(rx * rx + ry * ry + rz * rz);
+    auto rmag = M_PI_4 * NVLib::RandomUtils::GetInteger(0, 100) / 100.0;
+    return Vec3d(rx, ry, rz) / (rmag / rmag_r);
+}
+
+/**
+ * Get a random translation vector
+ * @return The random translation vector
+ */
+Vec3d GetRandomTranslation() 
+{
+    auto tx = NVLib::RandomUtils::GetInteger(-100, 100);
+    auto ty = NVLib::RandomUtils::GetInteger(-20, 20);
+    auto tz = NVLib::RandomUtils::GetInteger(200, 500);
+    return Vec3d(tx, ty, tz);
+}
+
+//--------------------------------------------------
+// Saving Logic
+//--------------------------------------------------
+
+/**
+ * Save the metadata to a file
+ * @param path The path to save the metadata
+ * @param metaData The metadata object
+ */
+void Save(const string& path, NVL_App::MetaData * metaData) 
+{
+    auto fs = FileStorage(path, FileStorage::WRITE | FileStorage::FORMAT_XML);
+    if (!fs.isOpened()) throw runtime_error("Failed to open file for writing: " + path);
+    
+    fs << "focal" << metaData->GetFocal();
+    fs << "imageSize" << metaData->GetImageSize();
+    fs << "rvec_1" << metaData->GetRVec_1();
+    fs << "tvec_1" << metaData->GetTVec_1();
+    fs << "rvec_2" << metaData->GetRVec_2();
+    fs << "tvec_2" << metaData->GetTVec_2();
+    fs << "blockSize" << metaData->GetBlockSize();
+    fs << "gridSize" << metaData->GetGridSize();
+
+    fs.release();
 }
 
 //--------------------------------------------------
