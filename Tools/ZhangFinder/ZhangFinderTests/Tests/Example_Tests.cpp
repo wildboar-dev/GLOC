b347846e7b8e3b4b75eeb8c458a2198c3de40bdd
Wildboar <trevorgeek@gmail.com>
2025-04-12 23:00:39 +0000

    Add the stub functionality for a tool for finding the calibration details from two homographies - using an approach similar to Zhang Calibration.

---
+++ Tools/ZhangFinder/ZhangFinderTests/Tests/Example_Tests.cpp
@@ -0,0 +1,21 @@
+//--------------------------------------------------
+// Unit Tests for Arguments
+//
+// @author: Wild Boar
+//
+// @date: 2025-04-12
+//--------------------------------------------------
+
+#include <gtest/gtest.h>
+
+//--------------------------------------------------
+// Test Methods
+//--------------------------------------------------
+
+/**
+ * @brief Confirm that the arguments are initialized correct
+ */
+TEST(Example_Test, test_example)
+{
+	ASSERT_EQ(1, 1);
+}
