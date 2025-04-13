73d8fb2628c0b5a7e4840374d174be5fb520f291
Wildboar <trevorgeek@gmail.com>
2025-04-13 00:06:07 +0000

    Add the stubs for the point loading logic

---
+++ Tools/ZhangFinder/ZhangFinderLib/PointLoader.cpp
@@ -0,0 +1,24 @@
+//--------------------------------------------------
+// Implementation of class PointLoader
+//
+// @author: Wild Boar
+//
+// @date: 2025-04-13
+//--------------------------------------------------
+
+#include "PointLoader.h"
+using namespace NVL_App;
+
+//--------------------------------------------------
+// Load
+//--------------------------------------------------
+
+/**
+ * @brief Add the functionality to load points from disk
+ * @param path The path that we are loading from
+ * @return unique_ptr<Points> Returns a unique_ptr<Points>
+ */
+unique_ptr<Points> PointLoader::Load(const string& path)
+{
+	throw runtime_error("Not implemented");
+}
---
+++ Tools/ZhangFinder/ZhangFinderTests/Tests/PointLoader_Tests.cpp
@@ -0,0 +1,32 @@
+//--------------------------------------------------
+// Unit Tests for class PointLoader
+//
+// @author: Wild Boar
+//
+// @date: 2025-04-13
+//--------------------------------------------------
+
+#include <gtest/gtest.h>
+
+#include <ZhangFinderLib/PointLoader.h>
+using namespace NVL_App;
+
+//--------------------------------------------------
+// Test Methods
+//--------------------------------------------------
+
+/**
+ * @brief Unit test to determine whether the load did as expected
+ */
+TEST(PointLoader_Test, confirm_load)
+{
+	FAIL() << "Not implemented";
+
+	// Setup
+
+	// Execute
+
+	// Confirm
+
+	// Teardown
+}
