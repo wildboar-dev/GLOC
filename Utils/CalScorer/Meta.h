7a76bd6a4aa11492ee8f16d9a2225e51a6799d80
Wildboar <trevorgeek@gmail.com>
2025-04-15 15:43:29 +1200

    Add the functionality to load the meta information from disk.

---
+++ Utils/CalScorer/Meta.h
@@ -0,0 +1,43 @@
+//--------------------------------------------------
+// The list of arguments that serve as input for the application
+//
+// @author: Wild Boar
+//
+// @date: 2024-03-06
+//--------------------------------------------------
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+namespace NVL_App
+{
+	class Meta
+	{
+	private:
+		double _focal;
+		Size _imageSize;
+		Vec3d _rvec_1;
+		Vec3d _tvec_1;
+		Vec3d _rvec_2;
+		Vec3d _tvec_2;
+		double _blockSize;
+		int _gridSize;
+	public:
+		Meta(double focal, Size& imageSize, Vec3d& rvec_1, Vec3d& tvec_1, Vec3d& rvec_2, Vec3d& tvec_2, double blockSize, int gridSize) :
+			_focal(focal), _imageSize(imageSize), _rvec_1(rvec_1), _tvec_1(tvec_1), _rvec_2(rvec_2), _tvec_2(tvec_2), _blockSize(blockSize), _gridSize(gridSize) {}
+
+		inline double& GetFocal() { return _focal; }
+		inline Size& GetImageSize() { return _imageSize; }
+		inline Vec3d& GetRvec_1() { return _rvec_1; }
+		inline Vec3d& GetTvec_1() { return _tvec_1; }
+		inline Vec3d& GetRvec_2() { return _rvec_2; }
+		inline Vec3d& GetTvec_2() { return _tvec_2; }
+		inline double& GetBlockSize() { return _blockSize; }
+		inline int& GetGridSize() { return _gridSize; }
+	};
+}
--- Utils/CalScorer/main.cpp
+++ Utils/CalScorer/main.cpp
@@ -14,6 +14,13 @@ using namespace std;
 #include <NVLib/Path/PathHelper.h>
 
 #include "ArgUtils.h"
+#include "PointLoader.h"
+#include "Meta.h"
+
+//--------------------------------------------------
+// Function Prototypes
+//--------------------------------------------------
+unique_ptr<NVL_App::Meta> LoadMeta(NVLib::PathHelper* pathHelper, const string& elementName); 
 
 //--------------------------------------------------
 // Main Execution Logic
@@ -31,14 +38,57 @@ void Execute(NVLib::Logger * logger, NVLib::Parameters * parameters)
     auto dataset = NVL_App::ArgUtils::GetString(parameters, "dataset");
     auto pathHelper = NVLib::PathHelper(database, dataset);
 
-    logger->Log(1, "Retrieving the element name");
+    logger->Log(1, "Retrieving the element name...");
     auto elementName = NVL_App::ArgUtils::GetString(parameters, "element");
     logger->Log(1, "Element Name: %s", elementName.c_str());
 
-    logger->Log(1, "Retrieviing the save file name");
+    logger->Log(1, "Retrieving the save file name...");
     auto saveFileName = NVL_App::ArgUtils::GetString(parameters, "save");
     logger->Log(1, "Save File Name: %s", saveFileName.c_str());
+
+    logger->Log(1, "Load test points...");
+    auto pointFile = stringstream(); pointFile << elementName << ".txt";
+    auto pointPath = pathHelper.GetPath("Point", pointFile.str());
+    auto points = NVL_App::PointLoader::Load(pointPath);
+    logger->Log(1, "Number of points Loaded: %i", points->GetScenePoints().size());
+
+    logger->Log(1, "Load meta information...");
+    auto meta = LoadMeta(&pathHelper, elementName);
+    logger->Log(1, "Focal: %f", meta->GetFocal());
+
 }
+
+//--------------------------------------------------
+// Loader Helpers
+//--------------------------------------------------
+
+/**
+ * @brief Load the meta information from the disk
+ * @param pathHelper The path helper to use
+ * @param elementName The name of the element
+ * @return unique_ptr<Meta> Returns a unique_ptr<Meta>
+ */
+unique_ptr<NVL_App::Meta> LoadMeta(NVLib::PathHelper* pathHelper, const string& elementName) 
+{
+    auto fileName = stringstream(); fileName << elementName << ".xml";
+    auto path = pathHelper->GetPath("Meta", fileName.str());
+
+    auto reader = FileStorage(path, FileStorage::FORMAT_XML | FileStorage::READ);
+    if (!reader.isOpened()) throw runtime_error("Unable to open: " + path);
+
+    double focal; reader["focal"] >> focal;
+    Size imageSize; reader["imageSize"] >> imageSize;
+    Vec3d rvec_1; reader["rvec_1"] >> rvec_1;
+    Vec3d tvec_1; reader["tvec_1"] >> tvec_1;
+    Vec3d rvec_2; reader["rvec_2"] >> rvec_2;
+    Vec3d tvec_2; reader["tvec_2"] >> tvec_2;
+    double blockSize; reader["blockSize"] >> blockSize;
+    int gridSize; reader["gridSize"] >> gridSize;
+  
+    reader.release();
+
+    return make_unique<NVL_App::Meta>(focal, imageSize, rvec_1, tvec_1, rvec_2, tvec_2, blockSize, gridSize);
+} 
 
 //--------------------------------------------------
 // Execution entry point
