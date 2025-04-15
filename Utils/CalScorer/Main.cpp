9d39e4e0c24fefca127b5ce2fb51ca51a875522c
Wildboar <trevorgeek@gmail.com>
2025-04-15 11:27:51 +0000

    Add the error calculation logic

--- Utils/CalScorer/Main.cpp
+++ Utils/CalScorer/Main.cpp
@@ -10,6 +10,8 @@
 using namespace std;
 
 #include <NVLib/Logger.h>
+#include <NVLib/Math2D.h>
+#include <NVLib/PoseUtils.h>
 #include <NVLib/Formatter.h>
 #include <NVLib/Path/PathHelper.h>
 
@@ -24,6 +26,9 @@ using namespace std;
 unique_ptr<NVL_App::Meta> LoadMeta(NVLib::PathHelper* pathHelper, const string& elementName);
 unique_ptr<NVL_App::Calibration> LoadCalibration(NVLib::PathHelper* pathHelper, const string& elementName);
 unique_ptr<NVL_App::Calibration> Meta2Calibration(NVL_App::Meta* meta); 
+Vec2d GetScore(NVL_App::Calibration * calibration, NVL_App::Points * points);
+void ProjectPoints(Mat& camera, Mat& pose, vector<Point3d>& scenePoints, vector<Point2d>& imagePoints);
+void FindDifferences(vector<Point2d>& expected, vector<Point2d>& actual, vector<double>& errors);
 
 //--------------------------------------------------
 // Main Execution Logic
@@ -65,6 +70,14 @@ void Execute(NVLib::Logger * logger, NVLib::Parameters * parameters)
     logger->Log(1, "Load calibration information...");
     auto measured_calibration = LoadCalibration(&pathHelper, elementName);
     logger->Log(1, "Load Success");
+
+    logger->Log(1, "Calculate the error with the truth calibration");
+    auto truth_error = GetScore(truth_calibration.get(), points.get());
+    logger->Log(1, "Error: %f +/- %f", truth_error[0], truth_error[1]);
+
+    logger->Log(1, "Calculate the error with the measured calibration");
+    auto measured_error = GetScore(measured_calibration.get(), points.get()); 
+    logger->Log(1, "Error: %f +/- %f", measured_error[0], measured_error[1]);
 }
 
 //--------------------------------------------------
@@ -144,15 +157,75 @@ unique_ptr<NVL_App::Calibration> Meta2Calibration(NVL_App::Meta* meta)
     camera.at<double>(1, 2) = meta->GetImageSize().height / 2;
     camera.at<double>(2, 2) = 1.0;
 
-    auto pose_1 = Mat(3, 4, CV_64F);
+    auto pose_1 = Mat(3, 4, CV_64F); auto plink_1 = (double *) pose_1.data;
     Rodrigues(meta->GetRvec_1(), pose_1.colRange(0, 3));
-    pose_1.col(3) = Mat(meta->GetTvec_1());
+    plink_1[3] = meta->GetTvec_1()[0];
+    plink_1[7] = meta->GetTvec_1()[1];
+    plink_1[11] = meta->GetTvec_1()[2];
 
-    auto pose_2 = Mat(3, 4, CV_64F);
+    auto pose_2 = Mat(3, 4, CV_64F); auto plink_2 = (double *) pose_2.data;
     Rodrigues(meta->GetRvec_2(), pose_2.colRange(0, 3));
-    pose_2.col(3) = Mat(meta->GetTvec_2());
+    plink_2[3] = meta->GetTvec_2()[0];
+    plink_2[7] = meta->GetTvec_2()[1];
+    plink_2[11] = meta->GetTvec_2()[2];
 
     return make_unique<NVL_App::Calibration>(camera, pose_1, pose_2);
+}
+
+//--------------------------------------------------
+// Get the associated score
+//--------------------------------------------------
+
+/**
+ * @brief Get the score of the calibration
+ * @param calibration The calibration information
+ * @param points The points to use
+ * @return Vec2d Returns a Vec2d with the score
+ */
+Vec2d GetScore(NVL_App::Calibration * calibration, NVL_App::Points * points) 
+{
+    auto errors = vector<double>();
+    
+    auto estimated_1 = vector<Point2d>(); ProjectPoints(calibration->GetCamera(), calibration->GetPose_1(), points->GetScenePoints(), estimated_1);
+    auto estimated_2 = vector<Point2d>(); ProjectPoints(calibration->GetCamera(), calibration->GetPose_2(), points->GetScenePoints(), estimated_2);
+    
+    FindDifferences(estimated_1, points->GetImagePoints_1(), errors);
+    FindDifferences(estimated_2, points->GetImagePoints_2(), errors);
+
+    auto mean = Scalar(), stddev = Scalar(); meanStdDev(errors, mean, stddev); 
+
+    return Vec2d(mean[0], stddev[0]);
+}
+
+/**
+ * Project the the associated points
+ * @param camera The camera that we are dealing with
+ * @param pose The pose that we are dealing with
+ * @param scenePoints The scene points that we are dealing with
+ * @param imagePoints The image points that we are dealing with
+ */
+void ProjectPoints(Mat& camera, Mat& pose, vector<Point3d>& scenePoints, vector<Point2d>& imagePoints) 
+{
+    auto rvec = Vec3d(), tvec = Vec3d(); NVLib::PoseUtils::Pose2Vectors(pose, rvec, tvec);
+    projectPoints(scenePoints, rvec, tvec, camera, noArray(), imagePoints);
+}
+
+/**
+ * Find the differences between elements
+ * @param expected The expected set of points
+ * @param actual The actual that we are dealing we are dealing with
+ * @param errors The errors that we are dealing with
+ */
+void FindDifferences(vector<Point2d> & expected, vector<Point2d>& actual, vector<double>& errors) 
+{
+    assert(expected.size() == actual.size());
+
+    for (auto i = 0; i < expected.size(); i++) 
+    {
+        auto difference = expected[i] - actual[i];
+        auto error = NVLib::Math2D::GetMagnitude(difference);
+        errors.push_back(error);
+    }
 }
 
 //--------------------------------------------------
