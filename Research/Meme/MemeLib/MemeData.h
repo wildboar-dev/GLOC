24aad58ec3db770133ef5f0848ad0a5b58d81197
Wildboar <trevorgeek@gmail.com>
2025-09-16 10:20:31 +1200

    Add the container that will hold the associated meta data

---
+++ Research/Meme/MemeLib/MemeData.h
@@ -0,0 +1,35 @@
+//--------------------------------------------------
+// A container for Meme Data
+//
+// @author: Wild Boar
+//
+// @date: 2025-09-16
+//--------------------------------------------------
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+namespace NVL_App
+{
+	class MemeData
+	{
+	private:
+		Mat _cameraMatrix;
+		Size _imageSize;
+		Size _gridSize;
+		double _blockSize;
+	public:
+		MemeData(Mat& cameraMatrix, Size& imageSize, Size& gridSize, double blockSize) :
+			_cameraMatrix(cameraMatrix), _imageSize(imageSize), _gridSize(gridSize), _blockSize(blockSize) {}
+
+		inline Mat& GetCameraMatrix() { return _cameraMatrix; }
+		inline Size& GetImageSize() { return _imageSize; }
+		inline Size& GetGridSize() { return _gridSize; }
+		inline double& GetBlockSize() { return _blockSize; }
+	};
+}
