ab32f7001b9b52cda1b67d340e2f2ac62204fae3
Wildboar <trevorgeek@gmail.com>
2025-09-24 16:40:28 +1200

    Start adding in the basic logic

---
+++ Research/MemeFinder/Mapper/Source.cpp
@@ -0,0 +1,92 @@
+//-----------------------------------------------------------------
+// Defines the C++ source file for generating cost function maps
+//
+// @author: 
+//-----------------------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+#include <MemeFinderLib/ArgUtils.h>
+#include <MemeFinderLib/ParameterFactory.h>
+#include <MemeFinderLib/PoseLoader.h>
+#include <MemeFinderLib/Parameters.h>
+
+#include <MemeLib/Points.h>
+#include <MemeLib/PointLoader.h>
+#include <MemeLib/MetaLoader.h>
+
+#include <NVLib/Path/PathHelper.h>
+
+#include "Logger.h"
+
+//-----------------------------------------------------------------
+// Prototypes
+//-----------------------------------------------------------------
+void Run(NVL_App::Logger & logger, NVLib::Parameters * parameters);
+
+//-----------------------------------------------------------------
+// Runner Method
+//-----------------------------------------------------------------
+
+/**
+ * @brief Runs the main logic of the Mapper application.
+ * @param logger Logger instance for logging messages
+ * @param parameters Parameters instance for configuration
+ */
+void Run(NVL_App::Logger & logger, NVLib::Parameters * parameters) 
+{
+    logger << NVL_App::Logger::Color(34) << "Loading Parameters" << NVL_App::Logger::Save();
+    auto database = NVL_App::ArgUtils::GetString(parameters, "database");
+    auto dataset = NVL_App::ArgUtils::GetString(parameters, "dataset");
+    auto pathHelper = NVLib::PathHelper(database, dataset);
+
+    logger << NVL_App::Logger::Color(34) << "Loading Points" << NVL_App::Logger::Save();
+    auto points = NVL_App::PointLoader::Load(pathHelper.GetPath("Point","point.txt"));
+    logger << "Loaded " << points->PointCount() << " points" << NVL_App::Logger::Save();
+
+    logger << NVL_App::Logger::Color(34) << "Loading Meta" << NVL_App::Logger::Save();
+    auto meta = NVL_App::MetaLoader::Load(pathHelper.GetPath("Meta","meta.xml"));
+
+    logger << NVL_App::Logger::Color(34) << "Loading Pose" << NVL_App::Logger::Save();
+    auto pose_1 = NVL_App::PoseLoader::LoadPose(pathHelper.GetPath("Meta","pose1.xml"));
+    auto pose_2 = NVL_App::PoseLoader::LoadPose(pathHelper.GetPath("Meta","pose2.xml"));
+
+    logger << NVL_App::Logger::Color(34) << "Create a parameters" << NVL_App::Logger::Save();
+    auto cx = meta->GetCameraMatrix().at<double>(0, 2); auto cy = meta->GetCameraMatrix().at<double>(1, 2);
+    auto scene = NVL_App::Parameters(Point2d(cx, cy)); scene.SetValue(0, -0.2); scene.SetValue(2, 0.03);
+
+}
+
+//-----------------------------------------------------------------
+// Entry Point
+//-----------------------------------------------------------------
+
+/**
+ * @brief Main entry point for the Mapper source file.
+ * @param argc Argument count
+ * @param argv Argument vector
+ * @return Exit status
+ */
+int main(int argc, char* argv[])
+{
+    auto logger = NVL_App::Logger();
+
+    try 
+    {
+        logger << NVL_App::Logger::Color(32) << "Starting Mapper Application" << NVL_App::Logger::Save();
+
+        auto parameters = NVL_App::ArgUtils::Load("MemeFinder", argc, argv);
+        Run(logger, parameters);
+        delete parameters;
+    }
+    catch (const exception& ex) 
+    {
+        logger << NVL_App::Logger::Color(31) << "Error: " << ex.what() << NVL_App::Logger::Save();
+        return EXIT_FAILURE;
+    }
+
+    logger << NVL_App::Logger::Color(32) << "Mapper Application Finished" << NVL_App::Logger::Save();
+
+    return EXIT_SUCCESS;
+}
