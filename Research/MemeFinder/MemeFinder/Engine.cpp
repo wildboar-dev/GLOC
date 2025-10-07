66f61da1e8bf3e4dda0b690b4c31e9b31a913ab6
Wildboar <trevorgeek@gmail.com>
2025-10-08 10:27:39 +1300

    Add the raw calibration data from an associated calibration + the stub for the logic to process this data.

--- Research/MemeFinder/MemeFinder/Engine.cpp
+++ Research/MemeFinder/MemeFinder/Engine.cpp
@@ -48,8 +48,7 @@ void Engine::Run(NVL_App::Logger & logger)
     //---------------------------------------------------
     // HARDCODED PARAMETERS
     //---------------------------------------------------
-    const double K1 = -0.2, K2 = 0.7;
-    const int dIndex_1 = 0, dIndex_2 = 1;
+    const auto testValues = Vec4d(0.1, -0.2, 0.6, 0.2);
     //---------------------------------------------------
 
     logger << NVL_App::Logger::Color(34) << "Loading Points" << NVL_App::Logger::Save();
@@ -65,8 +64,9 @@ void Engine::Run(NVL_App::Logger & logger)
 
     logger << NVL_App::Logger::Color(34) << "Create a parameters" << NVL_App::Logger::Save();
     auto cx = meta->GetCameraMatrix().at<double>(0, 2); auto cy = meta->GetCameraMatrix().at<double>(1, 2);
-    auto scene = NVL_App::Parameters(Point2d(cx, cy)); scene.SetValue(dIndex_1, K1); scene.SetValue(dIndex_2, K2);
-
+    auto scene = NVL_App::Parameters(Point2d(cx, cy)); 
+    for (auto i = 0; i < 4; i++) scene.SetValue(i, testValues[i]);
+    
     logger << NVL_App::Logger::Color(34) << "Applying the parameters to the basePoints" << NVL_App::Logger::Save();
     Mat camera = meta->GetCameraMatrix().clone(); Mat distortion = scene.GetDistortion();
     auto points = NVL_App::Utilities::ApplyDistortion(camera, pose_1.get(), pose_2.get(), &scene, basePoints.get());
@@ -82,7 +82,7 @@ void Engine::Run(NVL_App::Logger & logger)
     auto tracker = NVL_App::Tracker();
     //auto result = GradientDescent::Solve(&costFunction, x0, 1000, 1e-10, &tracker);
     //auto result = PSearch::Solve(&costFunction, x0, 100, 1e-2, 1e4, &tracker);
-    auto result = FPSearch::Solve(&costFunction, x0, 100, 1e-5, 1e4, 1e-21, &tracker);
+    auto result = FPSearch::Solve(&costFunction, x0, 1000, 1e-2, 1e4, DBL_EPSILON, &tracker);
 
     auto finalScore = costFunction.Evaluate(result);
     logger << NVL_App::Logger::Color(34) << "Final Score: " << finalScore << NVL_App::Logger::Save();
